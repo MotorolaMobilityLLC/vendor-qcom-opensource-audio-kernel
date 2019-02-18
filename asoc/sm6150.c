e44818a37d9d6666a7e410abfdf6578d26000b66
liaohj <liaohj@lenovo.com>
2019-02-18 12:15:41 +0800

    Correct some function use from qcom tag:4600
    correct some function
    Disable not use function :msm_ext_prepare_hifi
    update msm_tavil_snd_controls to msm_ext_snd_controls for qcom update name
    
    Change-Id: I486d508a50a317d92d7ce5b7db3467ce0f059da7
    Reviewed-on: https://gerrit.mot.com/1309980
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>

--- asoc/sm6150.c
+++ asoc/sm6150.c
@@ -8349,8 +8349,8 @@ int msm_cs47l35_init(struct snd_soc_pcm_runtime *rtd)
 		return ret;
 	}
 
-	ret = snd_soc_add_codec_controls(codec, msm_tavil_snd_controls,
-					 ARRAY_SIZE(msm_tavil_snd_controls));
+	ret = snd_soc_add_codec_controls(codec, msm_ext_snd_controls,
+					 ARRAY_SIZE(msm_ext_snd_controls));
 	if (ret < 0) {
 		pr_err("%s: add_codec_controls failed, err %d\n",
 			__func__, ret);
@@ -9805,6 +9805,7 @@ static void sm6150_ssr_disable(struct device *dev, void *data)
 	}
 }
 
+#ifndef CONFIG_SND_SOC_MADERA
 static int msm_ext_prepare_hifi(struct msm_asoc_mach_data *pdata)
 {
 	int ret = 0;
@@ -9831,6 +9832,7 @@ static int msm_ext_prepare_hifi(struct msm_asoc_mach_data *pdata)
 err:
 	return ret;
 }
+#endif
 
 static const struct snd_event_ops sm6150_ssr_ops = {
 	.enable = sm6150_ssr_enable,
