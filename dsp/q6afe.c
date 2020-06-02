1c8dd91f2214eec24a18f037107117bd03ef6558
a7301c <a7301c@motorola.com>
2020-06-01 22:01:43 -0500

    fixup (CR):audio-kernel:nairobi evb bringup kernel driver
    The fixup is required due to change:
    52721e7 ASOC: Speaker Protection V4
    
    Change-Id: I14cc4c5d7d4ad4a9a07794d9dba3e281d3b36b1b
    Signed-off-by: a7301c <a7301c@motorola.com>

--- dsp/q6afe.c
+++ dsp/q6afe.c
@@ -3602,7 +3602,8 @@ int aw_send_afe_rx_module_enable(void *buf, int cmd_size)
 	memcpy(&config, buf, cmd_size);
 
 	if (afe_spk_prot_prepare(port_id, 0,
-		AFE_PARAM_ID_AWDSP_RX_SET_ENABLE, &config)) {
+		AFE_PARAM_ID_AWDSP_RX_SET_ENABLE, &config,
+		  sizeof(union afe_spkr_prot_config))) {
 		pr_err("%s: set bypass failed \n", __func__);
 		return -EINVAL;
 	}
@@ -3620,7 +3621,8 @@ int aw_send_afe_tx_module_enable(void *buf, int cmd_size)
 	memcpy(&config, buf, cmd_size);
 
 	if (afe_spk_prot_prepare(port_id, 0,
-		AFE_PARAM_ID_AWDSP_TX_SET_ENABLE, &config)) {
+		AFE_PARAM_ID_AWDSP_TX_SET_ENABLE, &config,
+		  sizeof(union afe_spkr_prot_config))) {
 		pr_err("%s: set bypass failed \n", __func__);
 		return -EINVAL;
 	}
@@ -3772,7 +3774,7 @@ int aw_send_rx_module_enable(void *buf, int cmd_size)
 	memcpy(&config, buf, cmd_size);
 	if (afe_spk_prot_prepare(port_id, 0,
 			AFE_PARAM_ID_AWDSP_RX_SET_ENABLE,
-			&config)) {
+			&config, sizeof(union afe_spkr_prot_config))) {
 		pr_err("%s: AW set rx bypass failed\n",
 				   __func__);
 	}
@@ -3791,7 +3793,7 @@ int aw_send_tx_module_enable(void *buf, int cmd_size)
 	memcpy(&config, buf, cmd_size);
 	if (afe_spk_prot_prepare(port_id, 0,
 			AFE_PARAM_ID_AWDSP_TX_SET_ENABLE,
-			&config)) {
+			&config, sizeof(union afe_spkr_prot_config))) {
 		pr_err("%s: AW set tx module enable failed\n",
 				   __func__);
 	}
