5091cdbf007b6e72fde92116e5bfa519d639f64f
Ramu Gottipati <ramug@codeaurora.org>
2020-10-19 15:27:27 +0530

    ASoC: msm-routing: Add lock in lsm_app_type_cfg_control callbacks
    Add routing_lock mutex to protect global variables in _get() & _put()
    callback of Listen App Type Config mixer control
    
     CRs-Fixed: 2555096
     CVE-fixed: CVE-2020-11271
     Mot-CRs-fixed: (CR)
    
    Change-Id: I6857a6e72dcb7f21633919b716910577af4906d2
    Signed-off-by: Ramu Gottipati <ramug@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1854422
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- asoc/msm-pcm-routing-v2.c
+++ asoc/msm-pcm-routing-v2.c
@@ -14398,9 +14398,11 @@ static int msm_routing_put_lsm_app_type_cfg_control(
 	int i = 0, j;
 	int num_app_types;
 
+	mutex_lock(&routing_lock);
 	if (ucontrol->value.integer.value[0] > MAX_APP_TYPES) {
 		pr_err("%s: number of app types exceed the max supported\n",
 			__func__);
+		mutex_unlock(&routing_lock);
 		return -EINVAL;
 	}
 
@@ -14416,7 +14418,7 @@ static int msm_routing_put_lsm_app_type_cfg_control(
 		lsm_app_type_cfg[j].bit_width =
 				ucontrol->value.integer.value[i++];
 	}
-
+	mutex_unlock(&routing_lock);
 	return 0;
 }
 
