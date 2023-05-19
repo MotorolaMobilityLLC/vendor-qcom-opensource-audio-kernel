a3efad948c20b46cecfd9fc7d35931abb7d7d631
Lei Chen <chenlei18@lenovo.com>
2023-05-19 16:39:06 +0800

    Revert "asoc:add aw882xx codec driver"
    This reverts commit bcb242bb57ce28e557020632891c7a9b3786382b.

--- audio_kernel_modules.mk
+++ audio_kernel_modules.mk
@@ -72,7 +72,6 @@ AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko\
 	$(KERNEL_MODULES_OUT)/tx_macro_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/rx_macro_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wsa881x_analog_dlkm.ko \
-	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_slave_dlkm.ko
 endif	# bengal
