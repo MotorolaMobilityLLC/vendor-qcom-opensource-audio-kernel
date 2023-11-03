88d24d3a6fbaf879c75f86fc22297bc687a4d9d2
longc <longc156@motorola.com>
2023-11-03 16:57:45 +0800

    cusco:audio bringup
    details:
    -Add sw configuration
    
    Change-Id: I633e6c5d0f6df2dd780721bac7ca22449fd95261
    Reviewed-on: https://gerrit.mot.com/2784857
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <longc156@motorola.com>
    Reviewed-by: Zhengming Yao <yaozm1@lenovo.com>
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 2974936046de9a1dd07ccc0885a0125dee5737c3)
    Reviewed-on: https://gerrit.mot.com/3427882
    Reviewed-by: Monica Mabuchi <monicam@motorola.com>
    Reviewed-by: Andre Luis de Souza <alsouza@motorola.com>
    Reviewed-by: Gilberto Gambugge Neto <gambugge@motorola.com>

--- asoc/msm_dailink.h
+++ asoc/msm_dailink.h
@@ -383,6 +383,11 @@ SND_SOC_DAILINK_DEFS(tavil_i2s_tx1,
 	DAILINK_COMP_ARRAY(COMP_CODEC("tavil_codec", "tavil_i2s_tx1")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
+SND_SOC_DAILINK_DEFS(quin_mi2s_rx_cusco_fs19xx,
+	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("fs16xx.0-0034", "fs16xx-aif")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
+
 SND_SOC_DAILINK_DEFS(quin_mi2s_rx_aw882xx,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
 	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa_0", "aw882xx-aif-0"),
