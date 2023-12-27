bc51475036d58f84daba2055af8449742c2c73e9
dudd1 <dudd1@motorola.com>
2023-12-27 14:40:40 +0800

    wsa884x: dynamically set pa gain
    when DRE Off, dynamically set pa gain to wsa reg
    
    Change-Id: I34906aae00209029bdf6d1735ad219b1771e705d
    Reviewed-on: https://gerrit.mot.com/2837266
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Submit-Approved: Jira Key

--- asoc/codecs/wsa884x/wsa884x.c
+++ asoc/codecs/wsa884x/wsa884x.c
@@ -845,9 +845,9 @@ static int wsa_dev_mode_put(struct snd_kcontrol *kcontrol,
 }
 
 static const char * const wsa_pa_gain_text[] = {
-	"G_21_DB", "G_19P5_DB" "G_18_DB", "G_16P5_DB", "G_15_DB", "G_13P5_DB",
+	"G_21_DB", "G_19P5_DB", "G_18_DB", "G_16P5_DB", "G_15_DB", "G_13P5_DB",
 	"G_12_DB", "G_10P5_DB", "G_9_DB", "G_7P5_DB", "G_6_DB", "G_4P5_DB",
-	"G_3_DB", "G_1P5_DB", "G_0_DB", "G_M1P5_DB", "G_M3_DB", "G_M4P5_DB"
+	"G_3_DB", "G_1P5_DB", "G_0_DB", "G_M1P5_DB", "G_M3_DB", "G_M4P5_DB",
 	"G_M6_DB", "G_M7P5_DB", "G_M9_DB"
 };
 
@@ -880,7 +880,11 @@ static int wsa_pa_gain_put(struct snd_kcontrol *kcontrol,
 		__func__, ucontrol->value.integer.value[0]);
 
 	wsa884x->pa_gain =  ucontrol->value.integer.value[0];
-
+	if (!wsa884x->comp_enable) {
+		dev_dbg(component->dev, "%s: comp disable, dynamically set pa gain\n",__func__);
+		snd_soc_component_update_bits(component,
+			REG_FIELD_VALUE(DRE_CTL_1, CSR_GAIN, wsa884x->pa_gain));
+	}
 	return 0;
 }
 
