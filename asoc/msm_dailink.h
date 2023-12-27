49f781f3aae63c54265e1f94cd46edfd18321911
lbh <longc134@motorola.com>
2023-12-27 22:01:58 +0800

    Add fsm_update_dai_links
    add fsm_update_dai_links configs
    
    Change-Id: Ie0613252a6a3adc24b46da0b387d0b0711da5cec
    Reviewed-on: https://gerrit.mot.com/2837684
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yunxian.He <heyx6@motorola.com>
    Reviewed-by: Qiuying Lin <linqy3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3469372
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Monica Mabuchi <monicam@motorola.com>
    Reviewed-by: Gilberto Gambugge Neto <gambugge@motorola.com>

--- asoc/msm_dailink.h
+++ asoc/msm_dailink.h
@@ -384,13 +384,13 @@ SND_SOC_DAILINK_DEFS(tavil_i2s_tx1,
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(quin_mi2s_rx_cusco_fs19xx,
-	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("fs16xx.0-0034", "fs16xx-aif")),
-	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
+        DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
+        DAILINK_COMP_ARRAY(COMP_CODEC("fs16xx_0", "fs16xx-aif")),
+        DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(quin_mi2s_tx_cusco_fs19xx,
         DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-        DAILINK_COMP_ARRAY(COMP_CODEC("fs16xx.0-0034", "fs16xx-aif")),
+        DAILINK_COMP_ARRAY(COMP_CODEC("fs16xx_0", "fs16xx-aif")),
         DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(quin_mi2s_rx_aw882xx,
