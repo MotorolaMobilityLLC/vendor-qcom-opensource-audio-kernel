1210118346a0c9995e19cacb6cb2f1184719ef32
guoyf2 <guoyf2@lenovo.com>
2024-01-09 16:29:51 +0800

    mbhc: enble more debug log
    changes:
    1. for issue analysis easily, enable more debug log
    2. if zl or zr is 0,  set to line
    
    Change-Id: Ibb10901fa5a823e77c0c5eb6db63bd01dd1f9a93
    Signed-off-by: guoyf2 <guoyf2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2848909
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yunfei Guo <guoyf2@motorola.com>
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Submit-Approved: Jira Key

--- asoc/codecs/wcd-mbhc-adc.c
+++ asoc/codecs/wcd-mbhc-adc.c
@@ -637,6 +637,8 @@ static int wcd_mbhc_get_plug_from_adc(struct wcd_mbhc *mbhc, int adc_result)
 	hs_thr = wcd_mbhc_adc_get_hs_thres(mbhc);
 	hph_thr = wcd_mbhc_adc_get_hph_thres(mbhc);
 
+	pr_info("%s,adc_result=%d,hs_thr=%d,hph_thr=%d",__func__, adc_result, hs_thr, hph_thr);
+
 	if (adc_result < hph_thr)
 		plug_type = MBHC_PLUG_TYPE_HEADPHONE;
 	else if (adc_result > hs_thr)
@@ -665,7 +667,7 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 	int try = 0;
 	int hs_threshold, micbias_mv;
 
-	pr_debug("%s: enter\n", __func__);
+	pr_info("%s: enter\n", __func__);
 
 	mbhc = container_of(work, struct wcd_mbhc, correct_plug_swch);
 	component = mbhc->component;
@@ -686,7 +688,7 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 
 	if (cross_conn > 0) {
 		plug_type = MBHC_PLUG_TYPE_GND_MIC_SWAP;
-		pr_debug("%s: cross connection found, Plug type %d\n",
+		pr_info("%s: cross connection found, Plug type %d\n",
 			 __func__, plug_type);
 		goto correct_plug_type;
 	}
@@ -831,11 +833,11 @@ correct_plug_type:
 		}
 
 		if (output_mv > hs_threshold) {
-			pr_debug("%s: cable is extension cable\n", __func__);
+			pr_info("%s: cable is extension cable\n", __func__);
 			plug_type = MBHC_PLUG_TYPE_HIGH_HPH;
 			wrk_complete = true;
 		} else {
-			pr_debug("%s: cable might be headset: %d\n", __func__,
+			pr_info("%s: cable might be headset: %d\n", __func__,
 				 plug_type);
 			if (plug_type != MBHC_PLUG_TYPE_GND_MIC_SWAP) {
 				plug_type = wcd_mbhc_get_plug_from_adc(
@@ -860,7 +862,7 @@ correct_plug_type:
 				     (mbhc->current_plug !=
 				     MBHC_PLUG_TYPE_ANC_HEADPHONE) &&
 				    !wcd_swch_level_remove(mbhc)) {
-					pr_debug("%s: cable is %s headset\n",
+					pr_info("%s: cable is %s headset\n",
 						 __func__,
 						((spl_hs_count ==
 							WCD_MBHC_SPL_HS_CNT) ?
@@ -904,7 +906,7 @@ report:
 		goto exit;
 	}
 
-	pr_debug("%s: Valid plug found, plug type %d wrk_cmpt %d btn_intr %d\n",
+	pr_info("%s: Valid plug found, plug type %d wrk_cmpt %d btn_intr %d\n",
 			__func__, plug_type, wrk_complete,
 			mbhc->btn_press_intr);
 
@@ -984,7 +986,7 @@ exit:
 		mbhc->mbhc_cb->hph_pull_down_ctrl(component, true);
 
 	mbhc->mbhc_cb->lock_sleep(mbhc, false);
-	pr_debug("%s: leave\n", __func__);
+	pr_info("%s: leave\n", __func__);
 }
 
 static irqreturn_t wcd_mbhc_adc_hs_rem_irq(int irq, void *data)
@@ -995,7 +997,7 @@ static irqreturn_t wcd_mbhc_adc_hs_rem_irq(int irq, void *data)
 	bool hphpa_on = false;
 	u8  moisture_status = 0;
 
-	pr_debug("%s: enter\n", __func__);
+	pr_info("%s: enter\n", __func__);
 	WCD_MBHC_RSC_LOCK(mbhc);
 
 	timeout = jiffies +
@@ -1106,7 +1108,7 @@ static irqreturn_t wcd_mbhc_adc_hs_rem_irq(int irq, void *data)
 	}
 exit:
 	WCD_MBHC_RSC_UNLOCK(mbhc);
-	pr_debug("%s: leave\n", __func__);
+	pr_info("%s: leave\n", __func__);
 	return IRQ_HANDLED;
 }
 
@@ -1116,7 +1118,7 @@ static irqreturn_t wcd_mbhc_adc_hs_ins_irq(int irq, void *data)
 	u8 clamp_state = 0;
 	u8 clamp_retry = WCD_MBHC_FAKE_INS_RETRY;
 
-	pr_debug("%s: enter\n", __func__);
+	pr_info("%s: enter\n", __func__);
 
 	/*
 	 * ADC COMPLETE and ELEC_REM interrupts are both enabled for HEADPHONE,
@@ -1178,7 +1180,7 @@ static irqreturn_t wcd_mbhc_adc_hs_ins_irq(int irq, void *data)
 	mbhc->force_linein = false;
 	wcd_mbhc_adc_detect_plug_type(mbhc);
 	WCD_MBHC_RSC_UNLOCK(mbhc);
-	pr_debug("%s: leave\n", __func__);
+	pr_info("%s: leave\n", __func__);
 	return IRQ_HANDLED;
 }
 
