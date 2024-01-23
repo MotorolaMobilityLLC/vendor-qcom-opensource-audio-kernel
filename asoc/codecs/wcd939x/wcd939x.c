3032e97f99064ac7ad288b67656d4190edcfdeb7
huangys <huangys@motorola.com>
2024-01-23 13:58:57 +0800

    wcd939x: probe defer on wcd_reset returnging error
    wcd_reset may return error as wcd reset pin ctrl gpio data is null.
    This is because wcd reset gpio pin ctrl is not initialized completely.
    return -EPROBE_DEFER in order to get more change
    
    Change-Id: If6102f9948d5776cb1c6b451231daaba52a26543
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2862458
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dandan Du <dudd1@motorola.com>
    Submit-Approved: Jira Key

--- asoc/codecs/wcd939x/wcd939x.c
+++ asoc/codecs/wcd939x/wcd939x.c
@@ -4688,6 +4688,7 @@ static int wcd939x_reset(struct device *dev)
 	int rc = 0;
 	int value = 0;
 
+	pr_info("%s: enter\n", __func__);
 	if (!dev)
 		return -ENODEV;
 
@@ -4702,8 +4703,10 @@ static int wcd939x_reset(struct device *dev)
 	}
 
 	value = msm_cdc_pinctrl_get_state(wcd939x->rst_np);
-	if (value > 0)
+	if (value > 0) {
+		pr_info("%s: msm_cdc_pinctrl_get_state return %d", __func__, value);
 		return 0;
+	}
 
 	/* Set OVP threshold to 4.0V before reset */
 #if IS_ENABLED(CONFIG_QCOM_WCD_USBSS_I2C)
@@ -4733,6 +4736,7 @@ static int wcd939x_reset(struct device *dev)
 	wcd_usbss_set_ovp_threshold(VTH_4P2);
 #endif
 
+	pr_info("%s: exit, rc = %d\n", __func__, rc);
 	return rc;
 }
 
