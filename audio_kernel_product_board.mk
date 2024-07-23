5e8b862de00276797b19929a8fc88512d76da6c6
huangys <huangys@motorola.com>
2024-07-23 16:57:36 +0800

    tfa98xx: the initial commit
    - tfa98xx is smart pa driver from goodix, which uses i2s interface
      to connect QC chip
    - the driver code is all from vendor, only has a bit build failure fix
    - Add some kernel driver config file to support building and package
    
    Change-Id: I20b3b2596609d42e75a71e8Montana407a396377d058
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3037792
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- audio_kernel_product_board.mk
+++ audio_kernel_product_board.mk
@@ -41,6 +41,9 @@ PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/lpass_bt_swr_dlkm.ko
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/wcd9378_slave_dlkm.ko \
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/wcd9378_dlkm.ko \
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/qmp_dlkm.ko
+ifneq (,$(filter leap%, $(TARGET_PRODUCT)))
+PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/tfa98xx_dlkm.ko
+endif
 endif
 ifeq ($(call is-board-platform-in-list,bengal holi blair), true)
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/bolero_cdc_dlkm.ko \
