0432d12cd6d8ce1c68269ef30da084a1d8e6571c
huangys <huangys@motorola.com>
2024-07-24 19:47:58 +0800

    Asoc: sun: add tfa98xx quat i2s dailink
    Two tfa98xx smart boost chip are connected to AP for audio transfer.
    the stereo audio data uses this link
    
    Change-Id: I61e699d3f9c65cb6e9363e9922f3a0fd8e91306f
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3039363
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Submit-Approved: Jira Key

--- asoc/sun.c
+++ asoc/sun.c
@@ -1134,6 +1134,29 @@ static struct snd_soc_dai_link msm_mi2s_dai_links[] = {
 		.ignore_suspend = 1,
 		SND_SOC_DAILINK_REG(tert_mi2s_tx),
 	},
+#if IS_ENABLED(CONFIG_SND_SOC_TFA98XX)
+	{
+		.name = LPASS_BE_QUAT_MI2S_RX,
+		.stream_name = LPASS_BE_QUAT_MI2S_RX,
+		.playback_only = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.ops = &msm_common_be_ops,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1,
+		SND_SOC_DAILINK_REG(quat_mi2s_rx_tfa98xx),
+	},
+	{
+		.name = LPASS_BE_QUAT_MI2S_TX,
+		.stream_name = LPASS_BE_QUAT_MI2S_TX,
+		.capture_only = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.ops = &msm_common_be_ops,
+		.ignore_suspend = 1,
+		SND_SOC_DAILINK_REG(quat_mi2s_tx_tfa98xx),
+	},
+#else
 	{
 		.name = LPASS_BE_QUAT_MI2S_RX,
 		.stream_name = LPASS_BE_QUAT_MI2S_RX,
@@ -1155,6 +1178,7 @@ static struct snd_soc_dai_link msm_mi2s_dai_links[] = {
 		.ignore_suspend = 1,
 		SND_SOC_DAILINK_REG(quat_mi2s_tx),
 	},
+#endif
 	{
 		.name = LPASS_BE_QUIN_MI2S_RX,
 		.stream_name = LPASS_BE_QUIN_MI2S_RX,
