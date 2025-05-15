3ada1e5ad4bbc93e26ac439125871fd7b9e4c94f
huangys <huangys@motorola.com>
2025-05-15 17:37:36 +0800

    audio_machine: Urus(vantg) audio bringup
    - add tfa98xx i2s dailink
    - add dmic micbias gpio config
    
    Change-Id: I18a1f49f9b48848d99239bc55c663687ffe47d13
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3390910
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dandan Du <dudd1@motorola.com>
    Submit-Approved: Jira Key

--- asoc/audio_machine.c
+++ asoc/audio_machine.c
@@ -81,6 +81,7 @@ struct msm_asoc_mach_data {
 	struct device_node *dmic23_gpio_p; /* used by pinctrl API */
 	struct device_node *dmic45_gpio_p; /* used by pinctrl API */
 	struct device_node *dmic67_gpio_p; /* used by pinctrl API */
+	struct device_node *dmic_micbias_gpio_p; /* used by pinctrl API */
 	struct pinctrl *usbc_en2_gpio_p; /* used by pinctrl API */
 	bool is_afe_config_done;
 	struct device_node *wcd_usbss_handle;
@@ -95,6 +96,7 @@ struct msm_asoc_mach_data {
 	int wcd_used;
 };
 
+static int dmic_micbias_cnt;
 static bool is_initial_boot;
 static bool codec_reg_done;
 static struct snd_soc_card snd_soc_card_canoe_msm;
@@ -378,6 +380,20 @@ static int msm_dmic_event(struct snd_soc_dapm_widget *w,
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
+		if (pdata->dmic_micbias_gpio_p) {
+			dmic_micbias_cnt++;
+			dev_dbg(component->dev, "%s: dmic_micbias_cnt %d\n", __func__, dmic_micbias_cnt);
+			if (dmic_micbias_cnt == 1) {
+				dev_info(component->dev, "%s: enable micbias\n", __func__);
+				ret = msm_cdc_pinctrl_select_active_state(
+							pdata->dmic_micbias_gpio_p);
+				if (ret < 0) {
+					pr_err_ratelimited("%s: micbias gpio set cannot be activated %sd",
+						__func__, "pdata->dmic_micbias_gpio_p");
+					return ret;
+				}
+			}
+		}
 		(*dmic_gpio_cnt)++;
 		if (*dmic_gpio_cnt == 1) {
 			ret = msm_cdc_pinctrl_select_active_state(
@@ -391,6 +407,20 @@ static int msm_dmic_event(struct snd_soc_dapm_widget *w,
 
 		break;
 	case SND_SOC_DAPM_POST_PMD:
+		if (pdata->dmic_micbias_gpio_p) {
+			dmic_micbias_cnt--;
+			dev_dbg(component->dev, "%s: dmic_micbias_cnt %d\n", __func__, dmic_micbias_cnt);
+			if (dmic_micbias_cnt == 0) {
+				dev_info(component->dev, "%s: disable micbias\n", __func__);
+				ret = msm_cdc_pinctrl_select_sleep_state(
+							pdata->dmic_micbias_gpio_p);
+				if (ret < 0) {
+					pr_err_ratelimited("%s: micbias gpio set cannot be de-activated %sd",
+						__func__, "pdata->dmic_micbias_gpio_p");
+					return ret;
+				}
+			}
+		}
 		(*dmic_gpio_cnt)--;
 		if (*dmic_gpio_cnt == 0) {
 			ret = msm_cdc_pinctrl_select_sleep_state(
@@ -1103,6 +1133,29 @@ static struct snd_soc_dai_link msm_va_cdc_dma_be_dai_links[] = {
  * ------------------------------------
  */
 static struct snd_soc_dai_link msm_mi2s_dai_links[] = {
+#if IS_ENABLED(CONFIG_SND_SOC_TFA98XX)
+	{
+		.name = LPASS_BE_PRI_MI2S_RX,
+		.stream_name = LPASS_BE_PRI_MI2S_RX,
+		.playback_only = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.ops = &msm_common_be_ops,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1,
+		SND_SOC_DAILINK_REG(pri_mi2s_rx_tfa98xx),
+	},
+	{
+		.name = LPASS_BE_PRI_MI2S_TX,
+		.stream_name = LPASS_BE_PRI_MI2S_TX,
+		.capture_only = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.ops = &msm_common_be_ops,
+		.ignore_suspend = 1,
+		SND_SOC_DAILINK_REG(pri_mi2s_tx_tfa98xx),
+	},
+#else
 	{
 		.name = LPASS_BE_PRI_MI2S_RX,
 		.stream_name = LPASS_BE_PRI_MI2S_RX,
@@ -1124,6 +1177,7 @@ static struct snd_soc_dai_link msm_mi2s_dai_links[] = {
 		.ignore_suspend = 1,
 		SND_SOC_DAILINK_REG(pri_mi2s_tx),
 	},
+#endif
 	{
 		.name = LPASS_BE_SEC_MI2S_RX,
 		.stream_name = LPASS_BE_SEC_MI2S_RX,
@@ -2636,6 +2690,9 @@ static int msm_asoc_machine_probe(struct platform_device *pdev)
 	pdata->dmic67_gpio_p = of_parse_phandle(pdev->dev.of_node,
 					      "qcom,cdc-dmic67-gpios",
 					       0);
+	pdata->dmic_micbias_gpio_p = of_parse_phandle(pdev->dev.of_node,
+						  "qcom,dmic-micbias-en-gpio",
+						   0);
 	if (pdata->dmic01_gpio_p)
 		msm_cdc_pinctrl_set_wakeup_capable(pdata->dmic01_gpio_p, false);
 	if (pdata->dmic23_gpio_p)
@@ -2644,6 +2701,10 @@ static int msm_asoc_machine_probe(struct platform_device *pdev)
 		msm_cdc_pinctrl_set_wakeup_capable(pdata->dmic45_gpio_p, false);
 	if (pdata->dmic67_gpio_p)
 		msm_cdc_pinctrl_set_wakeup_capable(pdata->dmic67_gpio_p, false);
+	if (pdata->dmic_micbias_gpio_p) {
+		pr_info("%s: micbias gpio set\n", __func__);
+		msm_cdc_pinctrl_set_wakeup_capable(pdata->dmic_micbias_gpio_p, false);
+	}
 
 	msm_common_snd_init(pdev, card);
 
