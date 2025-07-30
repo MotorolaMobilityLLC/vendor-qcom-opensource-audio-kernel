cdee522b69dba3176347765308bcefb3a2a458d6
yaozm1 <yaozm1@lenovo.com>
2025-07-30 14:13:42 +0800

    asoc:add tfa98xx support for blanc
    add support for tfa98xx
    for blanc evb bringup
    
    Change-Id: I262b089169ba4189cf36dff10005cb6a35374dc3
    Signed-off-by: yaozm1 <yaozm1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3484998
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Qiuying Lin <linqy3@motorola.com>
    Submit-Approved: Jira Key

--- audio_kernel_modules.mk
+++ audio_kernel_modules.mk
@@ -56,7 +56,7 @@ endif
 ifeq ($(call is-board-platform-in-list,sun canoe alor whale chora),true)
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/lpass_bt_swr_dlkm.ko
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qmp_dlkm.ko
-ifneq (,$(filter vantg%, $(TARGET_PRODUCT)))
+ifneq (,$(filter vantg% blanc%, $(TARGET_PRODUCT)))
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/tfa98xx_dlkm.ko
 endif
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/simple_amp_dlkm.ko
